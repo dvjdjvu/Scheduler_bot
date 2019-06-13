a818013748472f0d82dd4dc0ca61b6a49a1b2d74
djvu <djvu@inbox.ru>
2019-06-13 15:20:01 +0300

    8

--- src/bot.py
+++ src/bot.py
@@ -20,11 +20,15 @@ class SvetaEyes():
         
         @self.bot.message_handler(commands=['start'])
         def get_start(message):
-            self.bot.send_message(message.chat.id, 'Привет, ты подключился ко мне.')
+            if not self.mongo.coll.find({"id": message.chat.id}).count() :
+                
+                self.bot.send_message(message.chat.id, 'Привет, ты подключился ко мне.')
             
-            self.mongo.coll.save({'id': message.chat.id, 'first_name': message.from_user.first_name, 'last_name': message.from_user.last_name})
+                self.mongo.coll.save({'id': message.chat.id, 'first_name': message.from_user.first_name, 'last_name': message.from_user.last_name})
             
-            print(message.chat.id, message.from_user.first_name, message.from_user.last_name)
+                print(message.chat.id, message.from_user.first_name, message.from_user.last_name)
+            else :
+                self.bot.send_message(message.chat.id, 'Привет, ты уже подключен ко мне.')
             
         @self.bot.message_handler(commands=['stop'])
         def get_start(message):
@@ -33,12 +37,11 @@ class SvetaEyes():
         
         @self.bot.message_handler(content_types=['text'])
         def get_text(message):
-            for men in self.mongo.coll.find({"id": message.chat.id}):
-                self.bot.send_message(message.from_user.id, "Привет ", men["name"])
-                
-                return
-            
-            self.bot.send_message(message.from_user.id, "Ты ко мне не подключен, напиши /start")
+            if self.mongo.coll.find({"id": message.chat.id}).count() :
+                for men in self.mongo.coll.find({"id": message.chat.id}):
+                    self.bot.send_message(message.from_user.id, "Привет ", men["name"])
+            else :
+                self.bot.send_message(message.from_user.id, "Ты ко мне не подключен, напиши /start")
     
     def __del__(self):
         pass
