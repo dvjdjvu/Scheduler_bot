1100c764fd49f339576aca070741f7c169731d89
djvu <djvu@inbox.ru>
2019-06-13 15:10:06 +0300

    4

--- src/bot.py
+++ src/bot.py
@@ -16,7 +16,29 @@ class SvetaEyes():
         self.mongo = _mongo
         
         self.keyboardStart = telebot.types.ReplyKeyboardMarkup()
-        self.keyboardStart.row('Подключиться', 'Не подключаться')        
+        self.keyboardStart.row('Подключиться', 'Не подключаться')
+        
+        @self.bot.message_handler(commands=['start'])
+        def get_start(message):
+            self.bot.send_message(message.chat.id, 'Привет, ты подключился ко мне.')
+            
+            self.mongo.coll.save({'id': message.chat.id, 'name': message.user.first_name})
+            
+            print(message.chat.id, message.user.first_name)
+            
+        @self.bot.message_handler(commands=['stop'])
+        def get_start(message):
+            self.bot.send_message(message.chat.id, 'Привет, ты отключился от меня.')
+            self.mongo.coll.remove({"id": message.chat.id})
+        
+        @self.bot.message_handler(content_types=['text'])
+        def get_text(message):
+            for men in self.mongo.coll.find({"id": message.chat.id}):
+                self.bot.send_message(message.from_user.id, "Привет ", men["name"])
+                
+                return
+            
+            self.bot.send_message(message.from_user.id, "Ты ко         мне не подключен, напиши /start")
     
     def __del__(self):
         pass
@@ -24,30 +46,7 @@ class SvetaEyes():
     def run(self):
         self.bot.polling(none_stop=True, interval=0)
     
-    @self.bot.message_handler(commands=['start'])
-    def get_start(message):
-        self.bot.send_message(message.chat.id, 'Привет, ты подключился ко мне.')
-        
-        self.mongo.coll.save({'id': message.chat.id, 'name': message.user.first_name})
-        
-        print(message.chat.id, message.user.first_name)
-        
-    @self.bot.message_handler(commands=['stop'])
-    def get_start(message):
-        self.bot.send_message(message.chat.id, 'Привет, ты отключился от меня.')
-        self.mongo.coll.remove({"id": message.chat.id})
     
-    
-    @self.bot.message_handler(content_types=['text'])
-    def get_text(message):
-        for men in self.mongo.coll.find({"id": message.chat.id}):
-            self.bot.send_message(message.from_user.id, "Привет ", men["name"])
-            
-            return
-        
-        self.bot.send_message(message.from_user.id, "Ты ко мне не подключен, напиши /start")
-        
-        return
     
             
             
