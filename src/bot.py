31a3dfe319d34e23a9c2fc1317610df65d1c96ca
djvu <djvu@inbox.ru>
2019-06-14 18:06:01 +0300

    10

--- src/bot.py
+++ src/bot.py
@@ -132,10 +132,9 @@ class SvetaEyes():
             tz = tzwhere.tzwhere()
             timezone_str = tz.tzNameAt(message.location.latitude, message.location.longitude)            
             
-            timezone = re.escape(pytz.timezone(timezone_str))
-            print(timezone, timezone.utcoffset(datetime.datetime.now()))
+            timezone = pytz.timezone(timezone_str)
             
-            self.mongo.coll.update({"id": message.chat.id}, {"$set": {"timezone": timezone, "timezone_offset": timezone.utcoffset(datetime.datetime.now())}})
+            self.mongo.coll.update({"id": message.chat.id}, {"$set": {"latitude": message.location.latitude, "longitude": message.location.longitude, "timezone_offset": timezone.utcoffset(datetime.datetime.now())}})
             
             for men in self.mongo.coll.find({"id": message.chat.id}):
                 print(men)            
