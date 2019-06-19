db4a31faf7dbc5edc3df1087a3a8ac4c79fe80c3
djvu <djvu@inbox.ru>
2019-06-19 19:24:17 +0300

    days

--- src/bot.py
+++ src/bot.py
@@ -248,15 +248,15 @@ class Sheduler():
         
         @self.bot.message_handler(commands=['ss'])
         def start(message):
-            markup = telebot.types.InlineKeyboardMarkup()
-            button = telebot.types.InlineKeyboardButton(text='CLick me', callback_data='add')
+            markup = types.InlineKeyboardMarkup()
+            button = types.InlineKeyboardButton(text='CLick me', callback_data='add')
             markup.add(button)
-            bot.send_message(chat_id=message.chat.id, text='Some text', reply_markup=markup)
+            self.bot.send_message(chat_id=message.chat.id, text='Some text', reply_markup=markup)
         
         @self.bot.callback_query_handler(func=lambda call: True)
         def query_handler(call):
             if call.data == 'add':
-                bot.answer_callback_query(callback_query_id=call.id, text='Hello world')        
+                self.bot.answer_callback_query(callback_query_id=call.id, text='Hello world')        
         
         @self.bot.message_handler(content_types=['text'])
         def get_text(message):
