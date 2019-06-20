afb7654dae32d712f9821bda0c536d094d8589df
djvu <djvu@inbox.ru>
2019-06-20 19:07:08 +0300

    days

--- src/bot.py
+++ src/bot.py
@@ -16,6 +16,88 @@ import ShedulerToken
 import json
 import re
 
+
+#!/usr/bin/python3
+from telegram.ext import Updater
+from telegram.ext import CommandHandler, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+############################### Bot ############################################
+def start(bot, update):
+    update.message.reply_text(main_menu_message(),
+                            reply_markup=main_menu_keyboard())
+
+def main_menu(bot, update):
+    query = update.callback_query
+    bot.edit_message_text(chat_id=query.message.chat_id,
+                        message_id=query.message.message_id,
+                        text=main_menu_message(),
+                        reply_markup=main_menu_keyboard())
+
+def first_menu(bot, update):
+    query = update.callback_query
+    bot.edit_message_text(chat_id=query.message.chat_id,
+                        message_id=query.message.message_id,
+                        text=first_menu_message(),
+                        reply_markup=first_menu_keyboard())
+
+def second_menu(bot, update):
+    query = update.callback_query
+    bot.edit_message_text(chat_id=query.message.chat_id,
+                        message_id=query.message.message_id,
+                        text=second_menu_message(),
+                        reply_markup=second_menu_keyboard())
+
+# and so on for every callback_data option
+def first_submenu(bot, update):
+    pass
+
+def second_submenu(bot, update):
+    pass
+
+############################ Keyboards #########################################
+def main_menu_keyboard():
+    keyboard = [[InlineKeyboardButton('Option 1', callback_data='m1')],
+              [InlineKeyboardButton('Option 2', callback_data='m2')],
+              [InlineKeyboardButton('Option 3', callback_data='m3')]]
+    return InlineKeyboardMarkup(keyboard)
+
+def first_menu_keyboard():
+    keyboard = [[InlineKeyboardButton('Submenu 1-1', callback_data='m1_1')],
+              [InlineKeyboardButton('Submenu 1-2', callback_data='m1_2')],
+              [InlineKeyboardButton('Main menu', callback_data='main')]]
+    return InlineKeyboardMarkup(keyboard)
+
+def second_menu_keyboard():
+    keyboard = [[InlineKeyboardButton('Submenu 2-1', callback_data='m2_1')],
+              [InlineKeyboardButton('Submenu 2-2', callback_data='m2_2')],
+              [InlineKeyboardButton('Main menu', callback_data='main')]]
+    return InlineKeyboardMarkup(keyboard)
+
+############################# Messages #########################################
+def main_menu_message():
+    return 'Choose the option in main menu:'
+
+def first_menu_message():
+    return 'Choose the submenu in first menu:'
+
+def second_menu_message():
+    return 'Choose the submenu in second menu:'
+
+############################# Handlers #########################################
+updater = Updater(ShedulerToken.token)
+
+updater.dispatcher.add_handler(CommandHandler('sss', start))
+updater.dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern='main'))
+updater.dispatcher.add_handler(CallbackQueryHandler(first_menu, pattern='m1'))
+updater.dispatcher.add_handler(CallbackQueryHandler(second_menu, pattern='m2'))
+updater.dispatcher.add_handler(CallbackQueryHandler(first_submenu,
+                                                    pattern='m1_1'))
+updater.dispatcher.add_handler(CallbackQueryHandler(second_submenu,
+                                                    pattern='m2_1'))
+
+updater.start_polling()
+
+'''
 class Sheduler():
     
     def __init__(self, _mongo):
@@ -241,7 +323,7 @@ class Sheduler():
             for men in self.mongo.coll.find({"id": message.chat.id}):
                 events = men.get('events', [])
                 for event in events:
-                    button = types.InlineKeyboardButton(text=event['name'], callback_data=json.dumps({'id': message.chat.id, 'type': 'event','name': event['name'], 'time': event['time']}))
+                    button = types.InlineKeyboardButton(text=event['name'], callback_data=json.dumps({'id': message.chat.id, 't': 'e'}))
                     markup.add(button)
                     
             self.bot.send_message(chat_id=message.chat.id, text='Ваши события', reply_markup=markup)
@@ -267,12 +349,14 @@ class Sheduler():
         def query_handler(call):
             call.data = json.loads(call.data)
             
-            if call.data['type'] == 'event' :
+            if call.data['t'] == 'e' :
                 #self.bot.answer_callback_query(callback_query_id=call.id, text=str(call.data['name']))
-                print(call.data)
-                print(call.id)
-                print(call.data['id'])
-                self.bot.send_message(chat_id=call.data['id'], text='Выбрано напоминание: {}'.format(call.data['name']))
+                
+                for men in self.mongo.coll.find({"id": call.data['id']}):
+                    events = men.get('events', [])
+                    for event in events:       
+                        self.bot.send_message(chat_id=call.data['id'], text='Выбрано напоминание: {}'.format(call.data['name']))
+                
         
         @self.bot.message_handler(content_types=['text'])
         def get_text(message):
@@ -311,11 +395,6 @@ class Sheduler():
             pass
         
         #call.data = json.loads(call.data)
-        '''
-        if call.data['type'] == 'event' :
-            #self.bot.answer_callback_query(callback_query_id=call.id, text=str(call.data['name']))
-            self.bot.send_message(chat_id=call.id, text='Выбрано напоминание: {}'.format(call.data['name']))
-        '''
         
     def run(self):
         self.threadTimer.start()
@@ -358,3 +437,5 @@ class Sheduler():
                             self.bot.send_message(men['id'], event['text'])
                 
             time.sleep(45)
+            
+'''
